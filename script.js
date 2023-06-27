a4772b2afe825c4aab9b55b854e601d02b2b256d
Sonu34668 <130197661+Sonu34668@users.noreply.github.com>
2023-06-27 08:51:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -20,16 +20,3 @@ console.log(decimalToBinary(33));  // Output: "100001"function decimalToBinary(d
     return '0';
   }
 
-  let binary = '';
-  while (decimal > 0) {
-    binary = (decimal % 2) + binary;
-    decimal = Math.floor(decimal / 2);
-  }
-
-  return binary;
-}
-
-// Example usage:
-console.log(decimalToBinary(7));   // Output: "111"
-console.log(decimalToBinary(10));  // Output: "1010"
-console.log(decimalToBinary(33));  // Output: "100001"
