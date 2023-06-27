6f07971b3a42337b58c7ef53af45aca6968f0e4a
Sonu34668 <130197661+Sonu34668@users.noreply.github.com>
2023-06-27 07:15:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,35 @@
-function decimalToBinary(decimal) {
-	let binary = '';
-	while(decimal > 0){
-		binary = (decimal % 2) + binary;
-		decimal = Math.floor(decimal / 2);
-	}
-return binary;
+// function decimalToBinary(decimal) {
+// 	let binary = '';
+// 	while(decimal > 0){
+// 		binary = (decimal % 2) + binary;
+// 		decimal = Math.floor(decimal / 2);
+// 	}
+// return binary;
 	
+// }
+
+// const readline = require("readline").createInterface({
+//   input: process.stdin,
+//   output: process.stdout,
+// });
+// const binary = decimalToBinary(decimal);
+// 	console.log(binary);
+
+function threeSum(arr, target) {
+     let binaryNum = new Array(32);
+ 
+   
+    let i = 0;
+    while (n > 0) {
+ 
+      
+        binaryNum[i] = n % 2;
+        n = Math.floor(n / 2);
+        i++;
+    }
+  binaryNum.reverse();
+	return binaryNum;
+    
 }
 
-const readline = require("readline").createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-const binary = decimalToBinary(decimal);
-	console.log(binary);
+module.exports = threeSum;
