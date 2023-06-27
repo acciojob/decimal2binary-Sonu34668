dcd380c31cc1abeb2d06fde3514507b6a364f473
Sonu34668 <130197661+Sonu34668@users.noreply.github.com>
2023-06-27 06:33:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -8,5 +8,9 @@ return binary;
 	
 }
 
+const readline = require("readline").createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 const binary = decimalToBinary(decimal);
 	console.log(binary);
