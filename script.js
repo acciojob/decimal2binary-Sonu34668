addfda124ebfc34290e3c1c4abf2d5da8d260c3d
Sonu34668 <130197661+Sonu34668@users.noreply.github.com>
2023-06-27 07:37:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,26 +1,3 @@
-// function decimalToBinary(decimal) {
-// 	let binary = '';
-// 	while(decimal > 0){
-// 		binary = (decimal % 2) + binary;
-// 		decimal = Math.floor(decimal / 2);
-// 	}
-// return binary;
-	
-// }
-
-// const readline = require("readline").createInterface({
-//   input: process.stdin,
-//   output: process.stdout,
-// });
-// const binary = decimalToBinary(decimal);
-// 	console.log(binary);
-
-const input = prompt('Please enter a decimal number:');
-const decimal = parseInt(input);
-
-if (isNaN(decimal)) {
-  console.log('Invalid input. Please enter a valid decimal number.');
-} else {
-  const binary = decimalToBinary(decimal);
-  console.log(`The binary equivalent of ${decimal} is ${binary}.`);
+function decimalToBinary(dec) {
+  return dec.toString(2);
 }
