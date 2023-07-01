b89ecf7c3a4eb9d2e3efaff9a5ac0e1648214937
Sonu34668 <130197661+Sonu34668@users.noreply.github.com>
2023-07-01 23:28:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,47 @@
-function toBinary(v, str) {
-  if (!Number.isSafeInteger(v) || v < 0) {
-    throw new Error('v must be a non-negative integer');
-  }
-  if (v === 1) {
-    return '1';
-  }
-  if (v === 0) {
-    return '0';
-  }
-  return toBinary(Math.floor(v / 2)) + (v % 2);
+function threeSum(arr, target) {
+// write your code here
+
+	arr.sort((a , b)=>a-b);
+
+	let n = arr.length;
+	let closestSum = Infinity;
+
+	for(let i=0; i<n-2; i++){
+		let left = i+1;
+		let right = n-1;
+		while(left<right){
+			let sum = arr[i]+arr[left]+arr[right];
+
+			if(Math.abs(target-sum) < Math.abs(target-closestSum)){
+				closestSum = sum;
+			}
+
+			if(sum < target){
+				left++;
+			}
+			else{
+				right--;
+			}
+		}
+	}
+		return closestSum;
+  
 }
+
+module.exports = threeSum;
+//  let s = "";
+// function number(n){
+// 	while(n>0){
+// 		s += n%2;
+// 		n = parseInt(n/2);
+// 	}
+// // 	return s;
+// }
+//   number(n);
+  
+//   let ans = ""
+// 	for(let i=s.length-1; i>=0; i--){
+// 	  ans += s[i];
+// 	}
+// console.log(ans);
+// module.exports = threeSum;
