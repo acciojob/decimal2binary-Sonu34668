c4a2f896369a437f43dfe3ecbcc2534189bb3798
Sonu34668 <130197661+Sonu34668@users.noreply.github.com>
2023-07-01 23:32:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,47 +1,15 @@
-function threeSum(arr, target) {
-// write your code here
+function number(num){
+    let add = "";
+        while(num > 0){
+            
+            let str = num%2;
+            add = str.toString() + add;
 
-	arr.sort((a , b)=>a-b);
-
-	let n = arr.length;
-	let closestSum = Infinity;
-
-	for(let i=0; i<n-2; i++){
-		let left = i+1;
-		let right = n-1;
-		while(left<right){
-			let sum = arr[i]+arr[left]+arr[right];
-
-			if(Math.abs(target-sum) < Math.abs(target-closestSum)){
-				closestSum = sum;
-			}
-
-			if(sum < target){
-				left++;
-			}
-			else{
-				right--;
-			}
-		}
-	}
-		return closestSum;
-  
-}
+            num =Math.floor(num/2);
+          
+        }
+        return add;
+    
+    }
 
 module.exports = threeSum;
-//  let s = "";
-// function number(n){
-// 	while(n>0){
-// 		s += n%2;
-// 		n = parseInt(n/2);
-// 	}
-// // 	return s;
-// }
-//   number(n);
-  
-//   let ans = ""
-// 	for(let i=s.length-1; i>=0; i--){
-// 	  ans += s[i];
-// 	}
-// console.log(ans);
-// module.exports = threeSum;
