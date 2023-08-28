4ca15f4409021552ac30d8120925f226a9fc49e2
Sonu34668 <130197661+Sonu34668@users.noreply.github.com>
2023-08-28 11:16:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function decimalToBinary(num) {
-  // Write your code here
+  const binary = [];
+  let remainder = 0;
+  let quotient = num;
+
+  while (quotient !== 0) {
+    remainder = quotient % 2;
+    binary.push(remainder);
+    quotient = Math.floor(quotient / 2);
+  }
+
+  binary.reverse();
+  return binary.join("");
 }
 
 window.decimalToBinary = decimalToBinary;
